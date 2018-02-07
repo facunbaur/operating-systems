0768770bb814bb385ad80ad4f1edf6fc26ba8e8f
root <root@localhost.localdomain>
2018-02-06 21:29:45 -0600

    Files we want

---
+++ cs161/cs161-gcc-1.5/gcc/.gdbinit
@@ -0,0 +1,3 @@
+dir .
+dir /cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc
+source /cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/gdbinit.in
---
+++ cs161/cs161-gcc-1.5/gcc/mddeps.mk
@@ -0,0 +1,64 @@
+MD_INCLUDES = \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/predicates.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4k.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5k.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/24k.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/3000.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4000.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4100.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4130.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4300.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4600.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5000.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5400.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5500.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/6000.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/7000.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/9000.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sb1.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sr71k.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/generic.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-ps-3d.md \
+	/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-dsp.md
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/predicates.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4k.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5k.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/24k.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/3000.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4000.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4100.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4130.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4300.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/4600.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5000.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5400.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/5500.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/6000.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/7000.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/9000.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sb1.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/sr71k.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/generic.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-ps-3d.md:
+
+/cs161/cs161-gcc-1.5/./gcc-4.1.2+cs161/gcc/config/mips/mips-dsp.md:
